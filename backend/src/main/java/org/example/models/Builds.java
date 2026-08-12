fa9bea18aa182c13c13bf57455d667b6a42b8956
Nicholas Beighley <37514789+Nick040791@users.noreply.github.com>
2026-08-12 17:51:29 -0500

    Created Models for Builds and Part Table

---
+++ backend/src/main/java/org/example/models/Builds.java
@@ -0,0 +1,66 @@
+package org.example.models;
+import java.time.LocalDateTime;
+
+public class Builds {
+    private int id;
+    private String name;
+    private String description;
+    private boolean is_public;
+    private String username;
+    private LocalDateTime created_at;
+
+    //Blank Constructor
+    public Builds(){}
+
+    //-----Getters and Setters----
+
+    //ID
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    //Name
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    //Description
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    //Is Public?
+    public boolean isIs_public() {
+        return is_public;
+    }
+    public void setIs_public(boolean is_public) {
+        this.is_public = is_public;
+    }
+
+    //Username
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    //Created at
+    public LocalDateTime getCreated_at() {
+        return created_at;
+    }
+    public void setCreated_at(LocalDateTime created_at) {
+        this.created_at = created_at;
+    }
+}
+
+
---
+++ backend/src/main/java/org/example/models/Part.java
@@ -0,0 +1,125 @@
+package org.example.models;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+//Part Model for DB
+public class Part
+{
+    private int id;
+    private String name;
+    private String category;
+    private String brand;
+    private String model;
+    private BigDecimal price;
+    private String description;
+    private boolean is_public;
+    private String username;
+    private LocalDateTime created_at;
+
+    //Blank constructor
+    public Part(){}
+
+    //---------Getters and Setters-----------
+
+    //ID
+    public int  getId()
+    {
+        return id;
+    }
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    //Name
+    public String getName()
+    {
+        return name;
+    }
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    //Category
+    public String getCategory()
+    {
+        return category;
+    }
+    public void setCategory(String category)
+    {
+        this.category = category;
+    }
+
+    //Brand
+    public String getBrand()
+    {
+        return brand;
+    }
+    public void setBrand(String brand)
+    {
+        this.brand = brand;
+    }
+
+    //Model
+    public String getModel()
+    {
+        return model;
+    }
+    public void  setModel(String model)
+    {
+        this.model = model;
+    }
+
+    //Price
+    public BigDecimal getPrice()
+    {
+        return price;
+    }
+    public void setPrice(BigDecimal price){
+        this.price = price;
+    }
+
+    //Description
+    public String getDescription()
+    {
+        return description;
+    }
+    public  void setDescription(String description)
+    {
+        this.description = description;
+    }
+
+    //Is Public?
+    public boolean isIs_public()
+    {
+        return is_public;
+    }
+    public void setIs_public(boolean is_public) {
+        this.is_public = is_public;
+    }
+
+    //Username
+    public String getUsername()
+    {
+        return username;
+    }
+    public void setUsername(String username)
+    {
+        this.username = username;
+    }
+
+    //Created at
+    public LocalDateTime getCreated_at()
+    {
+        return created_at;
+    }
+    public void setCreated_at(LocalDateTime created_at)
+    {
+        this.created_at = created_at;
+    }
+
+}
+
+
+
